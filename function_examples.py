2fe845444b9d2fa8a331e3ba1cde11d47a1078df
veragonzalez <veragonzalez2014@gmail.com>
2025-02-07 13:06:47 +0100

    Session 9&10

---
+++ function_examples.py
@@ -0,0 +1,54 @@
+from traceback import print_tb
+
+
+def greet():
+    """
+    Simple function printing hello
+    :return: None
+    """
+    print("Hello!")
+    return 0
+greet()
+x = greet()
+print(x)
+
+def greet_improved(name):
+    """
+    More complex greet that takes a name as param
+    :param name: the name of the person you greet
+    :return: None
+    """
+    print("Hello", name)
+
+greet_improved("John")
+greet_improved("Jane")
+
+def custom_op(x=0, y=0):
+    """
+    Custom op: 10x + y
+    :param x: the first number
+    :param y: the second number
+    :return: number, 10x+y
+    """
+    result = 10*x + y
+    return result
+
+print(custom_op(5, 8))
+x = custom_op( 5, 9 ) # arguments by position!
+print(f"the result of custom_op is {x}")
+x = custom_op(y=9, x=5) # arguments by name!
+print(f"the result of custom_op is: {x}")
+print(custom_op(5)) # default values for
+print(custom_op()) # default values for both
+print(custom_op(y=9)) # default value for x
+
+def bond_intro(name):
+    print("the name is:", name)
+
+def bond_name(first, last):
+    return f"{last}, {first} {last}"
+
+print(bond_name("Vera", "Gonzalez"))
+bond_intro(bond_name("Vera", "Gonzalez"))
+bond_intro(bond_name())
+
